cbbb6f214a4709c8ed24232eab57c137890cea7e
henriquepadua <henrique.lommes@gmail.com>
2022-06-24 17:02:56 -0300

    Adicionando Pesquisar

--- Doidona.java
+++ Doidona.java
@@ -79,5 +79,81 @@ class Doidona{
                 }
             }
         }    
-    }
-}
+    }//fim do Inserir
+
+    public boolean Pesquisar(int elemento){
+        boolean resp = false;
+        int pos = HT1(elemento);
+        if(T1[pos] == NULO){//se o elemento procurado de T1 for NULO resp recebe falso
+            resp = false;
+        }
+        else if(T1[pos] == true){//se nao se T1[pos] for encontrado resp recebe true
+            resp = true;
+        }else{//caso contrario
+            int h2 = HT2(elemento);
+            int R1h2 = r1T2(elemento);
+            int R2h2 = r2T2(elemento);
+            if(T2[h2] == NULO){//se a posicao do hash for NULO resp recebe falso
+                resp = false;
+            }
+            else if(T2[h2] == true){//se nao se T2[h2] for encontrado resp recebe true
+                resp = true;
+            }
+            if(T2[R1h2] == NULO){//se a posicao do primeiro rehash for NULO resp recebe false
+                resp = false;
+            }            
+            else if(T2[R1h2] == true){//se nao se T2[R1h2] for encontrado resp recebe true 
+                resp = true;
+            }
+            if(T2[R2h2] == NULO){//se a posicao do segundo rehash for NULO resp recebe falso
+                resp = false;
+            }
+            else if(T2[R2h2] == true){//se nao se T2[R1h2] for encontrado resp recebe true
+                resp = true;
+            }else{// caso contrario vai pesquisar na area de reserva de T2
+                boolean respAVL = avl.pesquisar(elemento);
+                if(respAVL == true){//se o elemento for encontrado na posicao reserva de T2 resp = true
+                    resp = true;
+                }else {//caso contrario vai pequisar na T3
+                    int h3 = HT3(elemento);
+                    if(T3[h3] == NULO){//se o elemento na posicao hash for NULO resp recebe falso
+                        resp = false;
+                    }else if(T3[h3] == true){//se nao se o elemento na posicao hash for encontrado resp recebe true
+                        resp = true;
+                    }else{// caso contrario vai procurar na area de reserva de T3
+                        boolean respRT3 =  (T3m + T3reserva < T3.length);
+                        if(T3[T3m + T3reserva] == NULO){// se o T3[T3m + T3reserva] for NULO resp recebe falso
+                            resp = false;
+                        }
+                        else if(respRT3 == false){//se nao se resp nao for encontrado na posicao T3[T3m + T3reserva] resp recebe false e soma uma posicao de reserva
+                            resp = pesquisarT3(elemento, T3reserva);// se ele encontrar o elemento na posicao de reserva for true resp recebe true se nao recebe falso
+                        }
+                        else if(respRT3 == true){//se nao se na primeira verificacao ja encontrar o elemento resp recebe true
+                            resp = true;
+                        }
+                    }
+                    int h4 = HT4(elemento);
+                    boolean PesquisarAlvinegra = AlvinegraT4[h4].pesquisar();
+                    if(AlvinegraT4[h4] == null || PesquisarAlvinegra == false){//se o meu elemento na posicao Alvinegra[h4] for null ou for falso resp recebe false
+                        resp = false;
+                    }else{//caso contrario resp recebe true
+                        resp = true;
+                    }      
+                }//fim da pesquisa em T3 e T4
+            }//fim da pesquisa em T2, T3 e T4
+        }         
+            return resp;
+    }//fim do Pesquisar
+
+    public boolean pesquisarT3(int elemento,int pos){
+        boolean resp = false;
+
+        if(T3[pos] == elemento){//se elemento for encontrado resp recebe true
+            resp = true;
+        }else if(T3[pos] != elemento){//enquanto ele nao encontrar o elemento chama recursivamente verifica a proxima posicao
+            resp = pesquisarT3(elemento, pos + 1);
+        }
+
+        return resp;
+    }// fim do metodo
+}//fim da class
