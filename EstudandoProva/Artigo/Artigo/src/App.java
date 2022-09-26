60fcf29ef65efc954742aede1b41f122c917be8f
henriquepadua <henrique.lommes@gmail.com>
2022-09-26 10:19:53 -0300

    segunda versao

---
+++ EstudandoProva/Artigo/Artigo/src/App.java
@@ -0,0 +1,10 @@
+public class App{
+    public static void main(String[] args) throws Exception {
+        Artigo artigo = new Artigo();
+        artigo.titulo = "Meu pe vermelho";
+        artigo.sumario = "1-19 Meu pe de toddy";
+        
+
+        System.out.println(artigo);
+    }
+}
