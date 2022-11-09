691b39370593a53f7566f5ab9f26353a2c5403ed
henriquepadua <henrique.lommes@gmail.com>
2022-11-09 19:38:31 -0300

    primeiros passos no spring boot

--- springTeste/java-spring-vscode-example/src/main/java/br/com/henriquepadua/javaspringvscodeexample/AppController.java
+++ springTeste/java-spring-vscode-example/src/main/java/br/com/henriquepadua/javaspringvscodeexample/AppController.java
@@ -1,25 +1,30 @@
 package br.com.henriquepadua.javaspringvscodeexample;
 
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@SpringBootTest
+@AutoConfigureMockMvc
 public class AppController {
     
-    @Value("${app.message}")
-    private String appmessage;
+    @Autowired
+    private MockMvc mvc;
 
-    @Value("${ENV_DB_URL:NULA}")
-    private String dbEnvironmentVariable;
+    @Test
+    public void getApp() throws Exception{
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(content().string(equalTo("Welcome to Spring Boot University")));
 
-    @GetMapping("/")
-    public String getAppMessage(){
-        return appmessage;
-    }
-
-    @GetMapping("/envVariable")
-    public String getEnvironmentVariable(){
-        return "A variavel de ambiente passada foi: " + dbEnvironmentVariable;
     }
 }
