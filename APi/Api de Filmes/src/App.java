c4e070a323b4b0e51316aa48017960d808e23ab5
henriquepadua <henrique.lommes@gmail.com>
2023-03-29 11:07:18 -0300

    Iniciando consumo de API

---
+++ APi/Api de Filmes/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        /* Iniciando consumo de api */
+
+        //fazer uma conexão HTTP e buscar os top 250 filmes
+
+        //extrair dados que interessam {titulo,poster,classificação}
+
+        //exibir e manipular os dados 
+    }
+}
